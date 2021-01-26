c67b635f144e94485e13712567305253b51619f1
Atharv Vallambhatla <atharv.val@gmail.com>
2021-01-25 19:09:23 -0700

    Add files via upload

---
+++ embbeded_valuues.py
@@ -0,0 +1,22 @@
+me = 'i am %s'
+print(me % 13)
+joke_text = '''%s:a device for finding furniture painfully in the dark'''
+a = 'toes'
+print(joke_text % a)
+spaces = ' ' * 25
+print('%s 12 butts wynd' % spaces)
+print('%s twinklebottom heath' % spaces)
+print('%s West Snoring' % spaces)
+print()
+print()
+print('dear sir')
+print()
+print('I wish to report that tiles are missing from the outside toilet roof')
+print('i think it was bad wind the other night that blew them away')
+print()
+print('regards')
+print('malcolm dithering')
+print(1000 * 'snirt')
+
+
+
